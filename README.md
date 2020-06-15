24745a65f407eeba2f33bf6318891bdc22f71312
Kyle Burke <k23burke@gmail.com>
2020-06-15 12:29:25 +0800

    link to blog post and cleanup of code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wedding-mailer
+
+I built this simple app to send invitations, reminders, and eventually thank you notes to those who were invited to my wedding. I have done my best to open-source it in a digestible way.
+
+There is an [accompanying blog post](https://medium.com/@oftenfrequent/send-automated-personalized-emails-for-free-fc4709663b36?sk=325f10a7078447354ce7ece8fafb1016) so that you can follow along in order to hook things up to your own personal gmail account and send emails from your terminal.
--- main.js
+++ main.js
@@ -7,17 +7,18 @@ const OAuth2 = google.auth.OAuth2;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const refreshToken = process.env.REFRESH_TOKEN;
+const gmailAccountAddress = process.env.FROM_EMAIL_ADDRESS;
 const emailTemplate = fs.readFileSync('./email.html', 'utf8')
 const guestlist = fs.readFileSync('./csv/sample.csv', 'utf8');
 
 
 const testMode = process.env.MAILER_ENV === 'test';
 
-const introAttended = `<p>This is the intro paragraph For those who attended. Notice the p tags around this content. That will separate this paragraph.</p>
-<p>From this one. You can also add links using an <a href="https://qz.com" target="_blank" rel="noopener noreferrer">achor tag.</a> Feel free to add your own stuff!</p>`;
+const introAttended = `<p>This is the intro paragraph For those who attended. Notice the p tags around this content, that will separate this paragraph.</p>
+<p>From this one. You can also add links using an <a href="https://qz.com" target="_blank" rel="noopener noreferrer">anchor tag.</a> Feel free to add your own stuff!</p>`;
 
-const introDidNotAttend = `<p>This is the intro paragraph For those who were not able to attend. Notice the p tags around this content. That will separate this paragraph.</p>
-<p>From this one. You can also add links using an <a href="https://qz.com" target="_blank" rel="noopener noreferrer">achor tag.</a> Feel free to add your own stuff!</p>`;
+const introDidNotAttend = `<p>This is the intro paragraph For those who were not able to attend. Notice the p tags around this content, that will separate this paragraph.</p>
+<p>From this one. You can also add links using an <a href="https://qz.com" target="_blank" rel="noopener noreferrer">anchor tag.</a> Feel free to add your own stuff!</p>`;
 
 const personalMessageDidNotAttend = `<p>This could be a generic message that is added for whatever reason.</p>`;
 
@@ -39,23 +40,24 @@ var main = async () => {
   // create nodemailer transporter
   try {
     transporter = nodemailer.createTransport({
-      service: 'mail',
+      service: 'gmail',
       auth: googleAuth,
     });
     await transporter.verify()
     console.log(chalk.green(`👍 Successfully created transporter 👍`))
   } catch (err) {
     console.log(chalk.red(`👎 Could not verify transporter 👎`))
-    console.log(chalk.white('Check out the docs at ', chalk.rgb(6,69,173).underline('https://nodemailer.com'), ' for more info'))
-    console.log(chalk.white(`Error: ${err}`))
+    console.log(chalk.white('Check out the docs at', chalk.rgb(6,69,173).underline('https://nodemailer.com'), 'for more info'))
+    console.log(chalk.white('Or read up on the community post connecting nodemailer with Gmail', chalk.rgb(6,69,173).underline('https://community.nodemailer.com/using-gmail/'),))
+    console.log(chalk.white(`${err}`))
     return
   }
 
   // send emails
   try {
     await sendEmailToGuestlist(transporter);
-    console.log(chalk.green(`Done!`))
-    return
+    console.log(chalk.green(`👍 Successfully sent emails 👍`))
+    return;
   } catch (err) {
     console.log(chalk.red(`👎 Something went wrong sending emails 👎`))
     console.log(chalk.white(`Error: ${err}`))
@@ -76,7 +78,7 @@ const getEmailsFromInfoArray = infoArray => {
 const getEmailContentFromInfoArray = infoArray => {
   const attendCount = parseInt(infoArray[5])
   if (isNaN(attendCount)) {
-    console.log(chalk.white(`Could not get attendance of ${infoArray[0]}`))
+    console.log(chalk.white(`Could not get attendance of: ${infoArray[0]}`))
     console.log(chalk.white(`Will skip for now...`))
   } else {
     let personalMessage = (infoArray[9] && infoArray[9].length) ? infoArray[9].replace(/["]+/g, '') : ''; // strip extra quotes
@@ -112,9 +114,6 @@ const sendEmailToGuestlist = async (transporter) => {
       if (emailString) {
         const emailContent = getEmailContentFromInfoArray(info);
         if (testMode) {
-          // console.log(chalk.white(`--------------------------------------------------`));
-          console.log(chalk.white(`Send To: ${emailString}`));
-          // console.log(chalk.white(`--------------------------------------------------`));
           return await sendEmail(emailContent, process.env.TEST_EMAIL_ADDRESS, transporter);
         } else {
           // send real email
@@ -123,39 +122,46 @@ const sendEmailToGuestlist = async (transporter) => {
 
       } else {
         // there is no email to send to...
-        console.log(chalk.blue(`NO EMAILs for ${info[0]}`))
-        return
+        console.log(chalk.blue(`Could not find an email address for: ${info[0]}`));
+        return;
       }
     });
   }, Promise.resolve())
-  console.log(chalk.white(`FINISHED WITH ${arrOfFailures.length} FAILURES:`))
-  console.log(chalk.white(arrOfFailures));
+
+  if (arrOfFailures.length) {
+    console.log(chalk.red(`Finished with ${arrOfFailures.length} failures:`))
+    console.log(chalk.white(arrOfFailures));
+  }
 }
 
 const getGoogleAuthObject = async () => {
-  var oauth2Client = new OAuth2(clientId, clientSecret, 'https://developers.google.com/oauthplayground');
-  oauth2Client.setCredentials({ refresh_token: refreshToken });
-
-  var tokens = await oauth2Client.refreshAccessToken();
-  var accessToken = tokens.credentials.access_token;
-
-  var googAuth = {
-    type: 'OAuth2',
-    user: process.env.FROM_EMAIL_ADDRESS,
-    clientId,
-    clientSecret,
-    refreshToken,
-    accessToken,
-  };
-
-  return googAuth;
+  try {
+    var oauth2Client = new OAuth2(clientId, clientSecret, 'https://developers.google.com/oauthplayground');
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
+  
+    var tokens = await oauth2Client.refreshAccessToken();
+    var accessToken = tokens.credentials.access_token;
+  
+    var googAuth = {
+      type: 'OAuth2',
+      user: gmailAccountAddress,
+      clientId,
+      clientSecret,
+      refreshToken,
+      accessToken,
+    };
+  
+    return googAuth;
+  } catch (err) {
+    throw err;
+  }
 }
 
 // make sending an email fault tolerant and synchronous 
 const sendEmail = async (emailHTML, toEmailString, transporter) => {
   return new Promise((res, rej) => {
     var mailOptions = {
-      from: `"${process.env.FROM_NAMES}" <${process.env.FROM_EMAIL_ADDRESS}>`,
+      from: `"${process.env.FROM_NAMES}" <${gmailAccountAddress}>`,
       to: toEmailString,
       subject: process.env.EMAIL_SUBJECT,
       html: emailHTML,
